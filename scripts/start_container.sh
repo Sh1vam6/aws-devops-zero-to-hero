88523ee70a92ef0f6bab32f7b80e5e4b4b843f8d
Shivam Gautam <97598721+Sh1vam6@users.noreply.github.com>
2024-04-02 01:33:36 +0530

    updating stop and start container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull sh1vam6/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 sh1vam6/simple-python-flask-app
